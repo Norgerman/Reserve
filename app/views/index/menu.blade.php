d0c4dceda71c0ecdab187b31180fbacb9363f682
CloudeTown <clouneatry@buaa.edu.cn>
2014-12-08 17:01:34 +0800

    让你们的主页不再出错

---
+++ app/views/index/menu.blade.php
@@ -0,0 +1,13 @@
+
+    <div class="menu">
+    <div class="submenu clearfix">
+        <ul id="menulist">
+        {{--@foreach ($menunames as $menuname)--}}
+        <li class="menublock">test</li>
+        <li class="menublock">test2</li>
+         <li class="menublock">test3</li>
+          <li class="menublock">test4</li>
+        {{--@endforeach--}}
+        </ul>
+    </div>
+    </div>
