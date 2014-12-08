75dc6f5c5a0c50cf627f92406348086e66d3fc1f
CloudeTown <clouneatry@buaa.edu.cn>
2014-12-08 17:16:54 +0800

    修改至有测试词汇

--- app/views/index/menu.blade.php
+++ app/views/index/menu.blade.php
@@ -1,13 +1,14 @@
+ <?php
+ $arr = array("首页","按医院预约","按科室预约","在线咨询","查询个人信息")
+
+ ?>
 
     <div class="menu">
     <div class="submenu clearfix">
         <ul id="menulist">
-        {{--@foreach ($menunames as $menuname)--}}
-        <li class="menublock">test</li>
-        <li class="menublock">test2</li>
-         <li class="menublock">test3</li>
-          <li class="menublock">test4</li>
-        {{--@endforeach--}}
+        @foreach ($arr as $menuname)
+        <li class="menublock">{{$menuname}}</li>
+        @endforeach
         </ul>
     </div>
     </div>
