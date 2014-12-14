7c092ad9d6d5dcb66421516b176a3f781084c8e1
vvliebe <vvliebe@gmail.com>
2014-12-14 21:51:09 +0800

    register user

--- public/js/register.js
+++ public/js/register.js
@@ -2,5 +2,7 @@
  * Created by vvliebe on 2014/12/14.
  */
 $(function () {
-    
+    $(".btn-group>.btn").click(function(){
+        $(this).addClass("active").parent().siblings().children().removeClass("active");
+    });
 })
