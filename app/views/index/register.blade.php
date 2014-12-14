8e6d4c15878b34638ab1305a89468bb0ae8bfa1f
vvliebe <vvliebe@gmail.com>
2014-12-14 20:19:11 +0800

    Less git Ignore

--- app/views/index/register.blade.php
+++ app/views/index/register.blade.php
@@ -12,8 +12,54 @@
 
 @section('body')
     @include('index.menu')
-    <div class="col-sm-12">
-        <div class="form-div">
-        </div>
+    <div class="col-sm-6 col-sm-offset-3 register-div">
+        <form role="form" class="form-horizontal col-sm-11">
+
+          <div class="form-group">
+            <label for="username" class="col-sm-2 control-label">用户名</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="username" placeholder="请输入用户名">
+            </div>
+          </div>
+
+          <div class="form-group">
+              <label for="password" class="col-sm-2 control-label">密码</label>
+              <div class="col-sm-10">
+                  <input type="password" class="form-control" id="password" placeholder="请输入密码">
+              </div>
+          </div>
+
+          <div class="form-group">
+              <label for="repassword" class="col-sm-2 control-label">确认密码</label>
+              <div class="col-sm-10">
+                  <input type="password" class="form-control" id="repassword" placeholder="再一次输入密码">
+              </div>
+          </div>
+
+          <div class="form-group">
+             <label for="tel" class="col-sm-2 control-label">手机</label>
+             <div class="col-sm-10">
+                 <input type="tel" class="form-control" id="tel" placeholder="请输入身份证号码">
+             </div>
+          </div>
+
+          <div class="form-group">
+            <label for="name" class="col-sm-2 control-label">真实姓名</label>
+            <div class="col-sm-10">
+               <input type="password" class="form-control" id="name" placeholder="请输入真实姓名">
+            </div>
+          </div>
+
+         <div class="form-group">
+             <label for="idnum" class="col-sm-2 control-label">身份证</label>
+             <div class="col-sm-10">
+                 <input type="password" class="form-control" id="idnum" placeholder="请输入身份证号码">
+             </div>
+         </div>
+
+           <div class="form-group">
+                <button type="submit" class="btn btn-default col-sm-offset-2 col-sm-3">注册</button>
+           </div>
+        </form>
     </div>
 @stop
---
+++ public/js/register.js
@@ -0,0 +1,6 @@
+/**
+ * Created by vvliebe on 2014/12/14.
+ */
+$(function () {
+    
+})
