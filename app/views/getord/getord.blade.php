ad1debf3a73833511724c1fcdc3c29449073ecb7
cobbb1 <531138675@qq.com>
2014-12-16 21:52:15 +0800

    111

---
+++ app/views/getord/getord.blade.php
@@ -0,0 +1,54 @@
+<div class="col-sm-3 go-div">
+    <div class="go seq">
+       <div class="go-title">
+           <label class="l1">预约流程</label>
+    </div>
+   </div>
+   <div class="go seldep">
+          <div class="go-title">
+              <label class="l2">选择科室</label>
+          </div>
+   </div>
+   <div class="go selhos">
+          <div class="go-title2">
+              <label class="l3">选择医院</label>
+          </div>
+          <div class="pan">
+             <div class="picture">
+               <img src="" />
+             </div>
+             <div class="name">
+               <label>name</label>
+             </div>
+             <div class="information">
+               <p>information</p>
+             </div>
+             <div class="hr1"></div>
+             <div class="ordpicture">
+               <img src = "" />
+             </div>
+             <div class="ordnumber">
+               <label>100</label>
+             </div>
+
+          </div>
+   </div>
+   <div class="go-pag">
+            <label class="pagelabelleft">首页</label>
+            <label class="pagelabelleft">上一页</label>
+            <label class="pagenumber">1</label>
+            <label class="pagenumber">2</label>
+            <label class="pagenumber">3</label>
+            <label class="pagenumber">4</label>
+            <label class="pagenumber">5</label>
+            <label class="pagenumber">6</label>
+            <label class="pagenumber">7</label>
+            <label class="pagenumber">8</label>
+            <label class="pagenumber">9</label>
+            <label class="pagenumber">10</label>
+            <label class="pagenumber">...</label>
+            <label class="pagelabelright">下一页</label>
+            <label class="pagelabelright">尾页</label>
+
+      </div>
+</div>
---
+++ app/views/getord/logo.blade.php
@@ -0,0 +1,5 @@
+<div class="logo-div col-sm-12">
+    <div class="logo">
+        <img src="images/logo.jpg" class="img-responsive" />
+    </div>
+</div>
