635eb2ffd46096486950f02da3d58c01eaa0764f
Norgerman <xyn0410@gmail.com>
2014-12-18 12:18:09 +0800

    deplist大改中......

--- app/controllers/DeplistController.php
+++ app/controllers/DeplistController.php
@@ -9,89 +9,43 @@
     class DeplistController
         extends BaseController
     {
-        private $depnum_perpage = 25;
+        private $docnum_perpage = 25;
 
-        public function  getDeplist()
+        public function  getDoclist()
         {
             $pagenum = Input::get("pagenum", 1);
-            
-            return Response::json($this->Dep($pagenum));
+
+            return Response::json();
         }
 
         public function  getIndex()
         {
-            return View::make("index.deplist", array("depinfo" => $this->Dep(1)));
+            $hos_id = Input::get("hos_id", 1);
+            Session::set("hos_id", $hos_id);
+            $departmentgroup = Department::where("hospital_id", "=", $hos_id)
+                                         ->get()
+                                         ->groupBy("class_id");
+
+            $firstgroup = $departmentgroup->first();
+            Session::set("department_id", $firstgroup[0]->d_id);
+
+            $departmentgroup = $departmentgroup->toArray();
+
+            foreach ($departmentgroup as $ind => $group)
+            {
+                foreach ($group as $index => $dep)
+                {
+                    $group[$index] = $dep->toArray();
+                }
+                $departmentgroup[$ind] = $group;
+            }
+            print_r($departmentgroup);
         }
 
-        private function Dep($pagenum)
+        private function Doc($pagenum)
         {
-            $result = array();
-            $start_num = $pagenum * $this->depnum_perpage - $this->depnum_perpage;
+            $department_id = Session::get("department_id");
             $isnew = false;
-
-            if (Session::has("class_id"))
-            {
-                $class_id = Session::get("class_id");
-            }
-            else
-            {
-                $class_id = 1;
-                Session::set("class_id", $class_id);
-                $isnew = true;
-            }
-
-            if (Input::has("class_id"))
-            {
-                $class_id = Input::get("class_id");
-                Session::set("class_id", $class_id);
-                $isnew = true;
-            }
-
-            if ($isnew)
-            {
-                $alldepcount = Department::where("class_id", "=", $class_id)
-                                         ->count();
-                $remain = $alldepcount % $this->depnum_perpage;
-                $pagecount = $alldepcount / $this->depnum_perpage;
-                if ($remain != 0)
-                {
-                    $pagecount = $pagecount + 1;
-                }
-                Session::set("deppagecount", $pagecount);
-            }
-            else
-            {
-                $pagecount = Session::get("deppagecount");
-            }
-
-            $deplist = Department::where("class_id", "=", $class_id)
-                                 ->skip($start_num)
-                                 ->take($this->depnum_perpage)
-                                 ->get();
-
-            $deparray = array();
-            foreach ($deplist as $index => $dep)
-            {
-                $dp = array();
-                $dp["class"] = $dep->depclass->name;
-                $dp["hosptial"] = $dep->hospital->name;
-                $dp["d_id"] = $dep->d_id;
-                $dp["name"] = $dep->name;
-                $dp["description"] = $dep->description;
-                $dp["tel"] = $dep->tel;
-                $deparray[$index] = $dp;
-            }
-
-            $depcount = count($deparray);
-
-            $res = array();
-            $res["count"] = $depcount;
-            $res["list"] = $deparray;
-            $res["pagenum"] = $pagenum;
-            $result["depinfo"] = $res;
-            $result["pagecount"] = (int)$pagecount;
-
-            return $result;
         }
 
     }
---
+++ app/controllers/QuestionController.php
@@ -0,0 +1,13 @@
+<?php
+
+    /**
+     * Created by PhpStorm.
+     * User: Norgerman
+     * Date: 12/18/14
+     * Time: 10:48 AM
+     */
+    class QuestionController
+        extends BaseController
+    {
+
+    }
