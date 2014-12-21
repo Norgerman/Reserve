733209a0f89684ff8b17f37ef608b80586ac2fcb
vvliebe <vvliebe@gmail.com>
2014-12-21 21:55:17 +0800

    换页面搞起

--- public/js/hospital.js
+++ public/js/hospital.js
@@ -2,19 +2,19 @@
  * Created by vvliebe on 2014/12/19.
  */
 $(function(){
-    $(".department li").click(function(){
-        $(this).addClass('active').addClass('isclick').siblings().removeClass('active').removeClass('isclick');
-
-    });
-    $(".department li").mouseenter(function(){
-        $(this).addClass('active').siblings().each(function(){
-            if(!$(this).hasClass('isclick')){
-                $(this).removeClass('active')
-            }
-        });
-    });
-    $(".department li").mouseout(function(){
-        if(!$(this).hasClass('isclick'))
-            $(this).removeClass('active');
-    });
+    //$(".department li").click(function(){
+    //    $(this).addClass('active').addClass('isclick').siblings().removeClass('active').removeClass('isclick');
+    //
+    //});
+    //$(".department li").mouseenter(function(){
+    //    $(this).addClass('active').siblings().each(function(){
+    //        if(!$(this).hasClass('isclick')){
+    //            $(this).removeClass('active')
+    //        }
+    //    });
+    //});
+    //$(".department li").mouseout(function(){
+    //    if(!$(this).hasClass('isclick'))
+    //        $(this).removeClass('active');
+    //});
 });
